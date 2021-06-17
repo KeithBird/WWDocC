4eaddc0bfea5e64963b3db661a12036e3ab2ff81
keith <K2968220169@outlook.com>
2021-06-17 11:03:10 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WWDocC
+
+A description of this package.
---
+++ Tests/WWDocCTests/WWDocCTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WWDocC
+
+final class WWDocCTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WWDocC().text, "Hello, World!")
+    }
+}
